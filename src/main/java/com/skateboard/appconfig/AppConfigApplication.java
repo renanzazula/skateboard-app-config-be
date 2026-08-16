eaddd261ddb4eed187a43b7f4de1b0416dfa9e23
renanzazula <renanzazula@gmail.com>
2026-08-16 20:47:31 +0200

    fix add missing app config application class

---
+++ src/main/java/com/skateboard/appconfig/AppConfigApplication.java
@@ -0,0 +1,12 @@
+package com.skateboard.appconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppConfigApplication.class, args);
+    }
+}
